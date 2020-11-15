3128e47a83cca47c52a231c705c861dc67fae31d
Lars Munkholm <lars.munkholm@akqa.com>
2020-11-15 17:28:55 +0100

    ✨ feat(string): add tools for YouTube IDs

---
+++ packages/string/src/convert/getYouTubeId.ts
@@ -0,0 +1,24 @@
+import { getYouTubeIdRegex } from "..";
+
+/**
+ * Convert a YouTube URL to a video ID.
+ *
+ * @example
+ * **Basic usage:**
+ * ```ts
+ * import { getYouTubeId } from "@baggie/string";
+ *
+ * const youTubeUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+ * const youTubeId = getYouTubeId(youTubeUrl);
+ * // youTubeId = "dQw4w9WgXcQ"
+ *
+ * const noYouTubeId = getYouTubeId("http://google.com");
+ * // noYouTubeId = ""
+ * ```
+ *
+ * @category Convert
+ */
+export function getYouTubeId(url: string) {
+    const match = url.match(getYouTubeIdRegex());
+    return match && match.length > 1 ? match[1] : "";
+}
--- packages/string/src/index.ts
+++ packages/string/src/index.ts
@@ -3,6 +3,7 @@ export { convertEmailsToLinks } from "./convert/convertEmailsToLinks";
 export { convertNewLinesToBreaks } from "./convert/convertNewLinesToBreaks";
 export { convertToDate } from "./convert/convertToDate";
 export { getEmailsFromString } from "./convert/getEmailsFromString";
+export { getYouTubeId } from "./convert/getYouTubeId";
 export { stripTags } from "./convert/stripTags";
 
 /* Interfaces */
@@ -16,6 +17,7 @@ export {
     passwordSpecialCharactersDefault,
 } from "./regex/getPasswordRegex";
 export { getTagsRegex } from "./regex/getTagsRegex";
+export { getYouTubeIdRegex } from "./regex/getYouTubeIdRegex";
 
 /* Validation */
 export { isEmailValid } from "./validation/isEmailValid";
