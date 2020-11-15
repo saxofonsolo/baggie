6055e6db983de8ea6901010850a67a786c34bc7d
Lars Munkholm <lars.munkholm@akqa.com>
2020-11-15 16:59:08 +0100

    ✨ feat(string): tools for html/xml tags

--- packages/string/src/index.ts
+++ packages/string/src/index.ts
@@ -2,6 +2,7 @@
 export { convertEmailsToLinks } from "./convert/convertEmailsToLinks";
 export { convertNewLinesToBreaks } from "./convert/convertNewLinesToBreaks";
 export { convertToDate } from "./convert/convertToDate";
+export { stripTags } from "./convert/stripTags";
 
 /* Interfaces */
 export type { PasswordOptions } from "./_interfaces/passwordOptions.interface";
@@ -13,6 +14,7 @@ export {
     getPasswordRegex,
     passwordSpecialCharactersDefault,
 } from "./regex/getPasswordRegex";
+export { getTagsRegex } from "./regex/getTagsRegex";
 
 /* Validation */
 export { isEmailValid } from "./validation/isEmailValid";
