567370adddbd0eea998fb7b3c8adfd4ad434a1dd
Lars Munkholm <lars.munkholm@akqa.com>
2020-11-15 15:57:51 +0100

    ✨ feat(string): add getNewLineRegex

--- packages/string/src/index.ts
+++ packages/string/src/index.ts
@@ -1,3 +1,4 @@
 /* Convert */
 export { convertNewLinesToBreaks } from "./convert/convertNewLinesToBreaks";
 export { convertToDate } from "./convert/convertToDate";
+export { getNewLineRegex } from "./regex/getNewLineRegex";
