857ea9e55981325bb9911fa265c7041a4ed9675e
Lars Munkholm <lars.munkholm@akqa.com>
2020-11-18 14:23:47 +0100

    ✅ test(string): add test for convertEmailsToLinks

---
+++ packages/string/src/convert/convertEmailsToLinks.test.ts
@@ -0,0 +1,45 @@
+import { convertEmailsToLinks } from "./convertEmailsToLinks";
+
+describe("convertEmailsToLinks", () => {
+    test("Convert a single email address", () => {
+        expect(
+            convertEmailsToLinks(`This text has a single email@address.com.`)
+        ).toBe(
+            `This text has a single <a href="mailto:email@address.com">email@address.com</a>.`
+        );
+    });
+
+    test("Convert a single email address with automatic subject", () => {
+        document.title = "Page title";
+        expect(
+            convertEmailsToLinks(
+                `This text has a single email@address.com.`,
+                true
+            )
+        ).toBe(
+            `This text has a single <a href="mailto:email@address.com?subject=Page title @ http://localhost/">email@address.com</a>.`
+        );
+    });
+
+    test("Convert a single email address with custom subject", () => {
+        document.title = "Page title";
+        expect(
+            convertEmailsToLinks(
+                `This text has a single email@address.com.`,
+                "test subject"
+            )
+        ).toBe(
+            `This text has a single <a href="mailto:email@address.com?subject=test subject">email@address.com</a>.`
+        );
+    });
+
+    test("Convert multiple email addresses", () => {
+        expect(
+            convertEmailsToLinks(
+                `Contact me at email@address.com or another-email@danish.dk.`
+            )
+        ).toBe(
+            `Contact me at <a href="mailto:email@address.com">email@address.com</a> or <a href="mailto:another-email@danish.dk">another-email@danish.dk</a>.`
+        );
+    });
+});
