bbc4cf0ec51e30dfc048d9cc3c91515fad806049
Lars Munkholm <mail@larsersej.dk>
2020-11-24 21:24:52 +0100

    ✨ feat(dom): add tools for getting/removing classes by postfix

---
+++ packages/dom/src/classes/removeClassByPostfix.ts
@@ -0,0 +1,14 @@
+import { getClassByPostfix } from "./getClassByPostfix";
+
+/**
+ * Remove all classes of an element that ends with a specified string.
+ *
+ * @category Classes
+ */
+export const removeClassByPostfix = (
+    element: Element = document.body,
+    prefix: string
+): void =>
+    getClassByPostfix(element, prefix).forEach((className) =>
+        element.classList.remove(className)
+    );
