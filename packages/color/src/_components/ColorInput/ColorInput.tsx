7ac805cef57613cdf38d7017fd0cc4818cfb6665
Lars (Mac) <lars@saxofonsolo.dk>
2023-04-08 10:49:09 +0200

    chore: slightly improve ColorInput component further

--- packages/color/src/_components/ColorInput/ColorInput.tsx
+++ packages/color/src/_components/ColorInput/ColorInput.tsx
@@ -5,11 +5,19 @@ import { convertColorNameToHex } from "../../convert/convertColorNameToHex";
 import { isHexColor } from "../../check/isHexColor/isHexColor";
 import S from "./ColorInput.module.scss";
 
-interface Props
-    extends Omit<
+type BaseProps = Omit<
+    React.DetailedHTMLProps<
+        React.InputHTMLAttributes<HTMLInputElement>,
+        HTMLInputElement
+    >,
+    "children" | "onChange" | "value" | "pattern" | "ref"
+> &
+    Omit<
         React.ComponentProps<typeof InputWrapper>,
         "label" | "labelForId" | "children"
-    > {
+    >;
+
+interface Props extends BaseProps {
     label: string;
     value: string;
     onChange: (value: string) => void;
@@ -18,54 +26,90 @@ interface Props
 export const ColorInput = React.forwardRef<HTMLInputElement, Props>(
     (props, ref) => {
         const [hasFocus, setHasFocus] = useState(false);
-        const id = useId();
+        const {
+            id,
+            onChange,
+            onBlur,
+            onFocus,
+            onKeyDown,
+            value,
+            label,
+            labelRight,
+            helperText,
+            invalid,
+            className,
+            ...inputProps
+        } = props;
+        const autoId = useId();
 
         const convertValue = useCallback(() => {
-            const trimmed = props.value.trim();
+            const trimmed = value.trim();
             const startsWithHash = trimmed.startsWith("#");
             if (!startsWithHash) {
                 const hex = convertColorNameToHex(trimmed) || `#${trimmed}`;
                 if (isHexColor(hex)) {
-                    props.onChange(hex);
+                    onChange(hex);
                 }
             }
-        }, [props]);
+        }, [onChange, value]);
 
-        const onKeyDown = useCallback(
+        const handleOnKeyDown = useCallback(
             (event: React.KeyboardEvent<HTMLInputElement>) => {
                 if (event.key === "Enter") {
                     convertValue();
                 }
+                if (onKeyDown) {
+                    onKeyDown(event);
+                }
             },
-            [convertValue],
+            [convertValue, onKeyDown],
         );
 
-        const onFocus = useCallback(() => {
-            setHasFocus(true);
-        }, []);
+        const handleOnFocus = useCallback(
+            (event: React.FocusEvent<HTMLInputElement, HTMLInputElement>) => {
+                setHasFocus(true);
+                if (onFocus) {
+                    onFocus(event);
+                }
+            },
+            [onFocus],
+        );
 
-        const onBlur = useCallback(() => {
-            setHasFocus(false);
-            convertValue();
-        }, [convertValue]);
+        const handleOnBlur = useCallback(
+            (event: React.FocusEvent<HTMLInputElement, HTMLInputElement>) => {
+                setHasFocus(false);
+                convertValue();
+                if (onBlur) {
+                    onBlur(event);
+                }
+            },
+            [convertValue, onBlur],
+        );
 
         return (
-            <InputWrapper label={props.label} labelForId={id}>
+            <InputWrapper
+                label={label}
+                labelForId={id || autoId}
+                labelRight={labelRight}
+                helperText={helperText}
+                invalid={invalid}
+            >
                 <input
                     ref={ref}
-                    id={id}
-                    value={props.value}
-                    onChange={({ target }) => props.onChange(target.value)}
-                    onFocus={onFocus}
-                    onBlur={onBlur}
-                    className={S.textInput}
-                    onKeyDown={onKeyDown}
+                    id={id || autoId}
+                    value={value}
+                    onChange={({ target }) => onChange(target.value)}
+                    onKeyDown={handleOnKeyDown}
+                    onFocus={handleOnFocus}
+                    onBlur={handleOnBlur}
+                    className={[S.textInput, className].join(" ").trim()}
                     pattern="^#((([0-9a-f]{3}){1,2})|(([0-9a-f]{4}){1,2}))$"
+                    {...inputProps}
                 />
                 <input
                     type="color"
-                    value={normalizeHexColor(props.value).substring(0, 7)}
-                    onChange={({ target }) => props.onChange(target.value)}
+                    value={normalizeHexColor(value).substring(0, 7)}
+                    onChange={({ target }) => onChange(target.value)}
                     className={S.colorInput}
                     tabIndex={hasFocus ? undefined : -1}
                 />
