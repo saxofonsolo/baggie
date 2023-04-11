e5b27d3f92059f26b2b254fcdb29f428a8987076
Lars (Mac) <lars@saxofonsolo.dk>
2023-04-11 22:58:24 +0200

    docs(functions): update docs for intersperse

--- packages/functions/src/arrays/intersperse/intersperse.ts
+++ packages/functions/src/arrays/intersperse/intersperse.ts
@@ -1,54 +1,5 @@
 /**
  * Insert new values between all values in an array.
- *
- * @example
- * **Basic usage:**
- * ```ts
- * import { intersperse } from "@baggie/functions";
- *
- * const numbers = [
- *     5,
- *     10,
- *     15,
- *     20,
- * ];
- * const newNumbers = intersperse(numbers, 1);
- * // newNumbers = [5, 1, 10, 1, 15, 1, 20]
- * ```
- *
- * @example
- * **Insert multiple:**
- * ```ts
- * import { intersperse } from "@baggie/functions";
- *
- * const numbers = [
- *     5,
- *     10,
- *     15,
- *     20,
- * ];
- * const newNumbersA = intersperse(numbers, 1, 2);
- * // newNumbersA = [5, 1, 2, 10, 1, 2, 15, 1, 2, 20]
- *
- * const newNumbersB = intersperse(numbers, ...["one", "two"]);
- * // newNumbersB = [5, "one", "two", 10, "one", "two", 15, "one", "two", 20]
- * ```
- *
- * @example
- * **With a function:**
- * ```ts
- * import { intersperse } from "@baggie/functions";
- *
- * const numbers = [
- *     10,
- *     20,
- *     30,
- *     40,
- * ];
- * const insertion = ({ previous, next }) => previous + (next - previous) / 2;
- * const newNumbers = intersperse(numbers, insertion);
- * // newNumbers = [10, 15, 20, 25, 30, 35, 40]
- * ```
  */
 export const intersperse = (array: any[], ...insertion: any[]): any[] =>
     array.reduce((accumulator: any[], element, index, array) => {
