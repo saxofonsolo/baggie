700f6ad2cca1a361eb2b1349ab8c928e8494b87f
Lars (Mac) <lars@saxofonsolo.dk>
2023-04-12 10:58:05 +0200

    chore(functions): update function type for insertion in intersperse

--- packages/functions/src/arrays/intersperse/intersperse.ts
+++ packages/functions/src/arrays/intersperse/intersperse.ts
@@ -1,21 +1,21 @@
+type Insertion =
+    | Omit<any, "function">
+    | ((args: { previous: any; next: any; index: number }) => any);
+
 /**
  * Insert new values between all values in an array.
  */
-export const intersperse = (array: any[], ...insertion: any[]): any[] =>
+export const intersperse = (array: any[], ...insertion: Insertion[]): any[] =>
     array.reduce((accumulator: any[], element, index, array) => {
         accumulator.push(element);
         if (index < array.length - 1) {
-            insertion.forEach((insert: any) =>
+            insertion.forEach((insert, insertIndex) =>
                 accumulator.push(
                     typeof insert === "function"
-                        ? (
-                              insert as (args: {
-                                  previous: any;
-                                  next: any;
-                              }) => any
-                          )({
+                        ? insert({
                               previous: array[index],
                               next: array[index + 1],
+                              index: insertIndex,
                           })
                         : insert,
                 ),
