9a28b24561b47f409236e3ae93e38421146fc1fb
Lars (Mac) <lars@saxofonsolo.dk>
2023-04-29 13:57:00 +0200

    chore: remove legacy jsdoc comment

--- packages/math/src/range/keepOutsideRange/keepOutsideRange.ts
+++ packages/math/src/range/keepOutsideRange/keepOutsideRange.ts
@@ -3,39 +3,6 @@ import { Range } from "../_interfaces/range.interface";
 
 /**
  * Make sure a number is outside the range of a minimum and maximum value.
- *
- * If the number is inside the range, the nearest accepted value will be
- * returned.
- *
- * @example
- * **Basic usage:**
- * ```ts
- * import { keepOutsideRange } from "@baggie/math";
- *
- * const smallNumber = keepOutsideRange(30, 25, 75);
- * // smallNumber = 25
- *
- * const largeNumber = keepOutsideRange(70, 25, 75);
- * // largeNumber = 75
- *
- * const smallOutsideNumber = keepOutsideRange(1, 25, 75);
- * // smallOutsideNumber = 1
- *
- * const largeOutsideNumber = keepOutsideRange(100, 25, 75);
- * // largeOutsideNumber = 100
- * ```
- *
- * @example
- * **When the number is the midpoint in the range:**
- * ```ts
- * import { keepOutsideRange } from "@baggie/math";
- *
- * const roundUp = keepOutsideRange(5, 0, 10);
- * // roundUp = 10
- *
- * const roundDown = keepOutsideRange(5, 0, 10, true);
- * // roundDown = 0
- * ```
  */
 export const keepOutsideRange = (
     number: number,
