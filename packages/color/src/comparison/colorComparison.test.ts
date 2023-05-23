fbc9f05da55c3364d4691d09fae095ac691f2113
Lars (Mac) <lars@saxofonsolo.dk>
2023-05-23 20:08:44 +0200

    test(color): fix test for ColorComparison

--- packages/color/src/comparison/colorComparison.test.ts
+++ packages/color/src/comparison/colorComparison.test.ts
@@ -3,106 +3,30 @@ import { ColorComparison } from "./colorComparison";
 describe("ColorComparison", () => {
     test("Red", () => {
         const colors = new ColorComparison([
-            {
-                red: 255,
-                green: 0,
-                blue: 0,
-            },
-            {
-                red: 255,
-                green: 255,
-                blue: 0,
-            },
-            {
-                red: 255,
-                green: 0,
-                blue: 255,
-            },
-            {
-                red: 0,
-                green: 255,
-                blue: 255,
-            },
-            {
-                red: 0,
-                green: 255,
-                blue: 0,
-            },
+            "#ff0000",
+            "#ffff00",
+            "#ff00ff",
+            "#00ffff",
+            "#00ff00",
         ]);
 
-        colors.add({
-            red: 255,
-            green: 50,
-            blue: 0,
-        });
+        colors.add("#ff8000");
 
-        expect(
-            colors.nearest({
-                red: 255,
-                green: 60,
-                blue: 10,
-            }),
-        ).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    red: 255,
-                    green: 50,
-                    blue: 0,
-                }),
-            ]),
-        );
-
-        expect(
-            colors.farthest(
-                {
-                    red: 255,
-                    green: 50,
-                    blue: 50,
-                },
-                2,
-            ),
-        ).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    red: 0,
-                    green: 255,
-                    blue: 0,
-                }),
-                expect.objectContaining({
-                    red: 0,
-                    green: 255,
-                    blue: 255,
-                }),
-            ]),
-        );
-
-        colors.reset().add([
-            {
-                red: 255,
-                green: 50,
-                blue: 50,
-            },
-            {
-                red: 0,
-                green: 255,
-                blue: 0,
-            },
+        expect(colors.nearest("#ff9010")).toStrictEqual([
+            "#ff8000",
+            "#ff0000",
+            "#ffff00",
+            "#00ffff",
+            "#00ff00",
+            "#ff00ff",
         ]);
 
-        expect(
-            colors.nearest({
-                red: 255,
-                green: 0,
-                blue: 0,
-            }),
-        ).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    red: 255,
-                    green: 50,
-                    blue: 50,
-                }),
-            ]),
+        expect(colors.farthest("#ff4040", 2)).toEqual(
+            expect.arrayContaining(["#00ff00", "#00ffff"]),
         );
+
+        colors.reset().add(["#ff0000", "#907ff0"]);
+
+        expect(colors.nearest("#ff4040")).toStrictEqual(["#ff0000", "#907ff0"]);
     });
 });
