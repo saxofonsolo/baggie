cf3b504f7878a8c37870ae896ec47266b2b85fa1
Lars (Mac) <lars@saxofonsolo.dk>
2023-06-01 10:08:48 +0200

    feat(react): add hook useKeyboardHeight

---
+++ packages/react/src/hooks/useKeyboardHeight/useKeyboardHeight.ts
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from "react";
+
+export const useKeyboardHeight = (customProperty: boolean | string = false) => {
+    const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+    const onResizeVisualViewport = useCallback(() => {
+        const newHeight =
+            window.innerHeight -
+            (window.visualViewport?.height || window.innerHeight);
+
+        setKeyboardHeight(newHeight);
+        if (customProperty) {
+            document.documentElement.style.setProperty(
+                (typeof customProperty === "string"
+                    ? customProperty
+                    : "keyboard-height"
+                ).replace(/^-{0,2}/, "--"),
+                `${newHeight}px`,
+            );
+        }
+    }, [customProperty]);
+
+    useEffect(() => {
+        window.visualViewport?.addEventListener(
+            "resize",
+            onResizeVisualViewport,
+        );
+
+        return () =>
+            window.visualViewport?.removeEventListener(
+                "resize",
+                onResizeVisualViewport,
+            );
+    }, [onResizeVisualViewport]);
+
+    return keyboardHeight;
+};
