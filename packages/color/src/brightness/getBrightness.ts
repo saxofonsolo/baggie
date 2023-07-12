0ba007c4de4e90bc9c3d37b0a6f967197b8a425a
Lars (Mac) <lars@saxofonsolo.dk>
2023-07-12 20:43:17 +0200

    replace getBrightness login with import from core

--- packages/color/src/brightness/getBrightness.ts
+++ packages/color/src/brightness/getBrightness.ts
@@ -1,3 +1,4 @@
+import { convertRgbToHex, getColorBrightness } from "@baggie/core";
 import { RGBA } from "../_interfaces/rgba.interface";
 
 /**
@@ -8,11 +9,5 @@ import { RGBA } from "../_interfaces/rgba.interface";
  * @category Brightness
  * @returns A number from 0 to 1, 1 being the brightest and 0 being not bright at all.
  */
-export const getBrightness = ({ red, green, blue }: RGBA): number => {
-    const perceivedBrightness = Math.sqrt(
-        0.299 * (red * red) + 0.587 * (green * green) + 0.114 * (blue * blue)
-    );
-
-    // Return perceived brightness as percentage in decimal form
-    return perceivedBrightness / 2.55 / 100;
-};
+export const getBrightness = (rgb: RGBA) =>
+    getColorBrightness(convertRgbToHex(rgb));
