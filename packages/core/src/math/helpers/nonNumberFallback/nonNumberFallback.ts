dc66902ac3bdc516cb2cd094d0a18562e498349b
Lars (Mac) <lars@saxofonsolo.dk>
2023-08-04 11:48:10 +0200

    fix(core): fix bug in nonNumberFallback

--- packages/core/src/math/helpers/nonNumberFallback/nonNumberFallback.ts
+++ packages/core/src/math/helpers/nonNumberFallback/nonNumberFallback.ts
@@ -1,9 +1,11 @@
 /**
  * Return fallback if input is not and can't be converted into a number.
  */
-export const nonNumberFallback = (value: any, fallback: number): number =>
-    typeof value === "number"
-        ? value
-        : typeof value === "string"
-        ? parseFloat(value) || fallback
-        : fallback;
+export const nonNumberFallback = (value: any, fallback: number): number => {
+    if (typeof value === "number") return value;
+    if (typeof value === "string") {
+        const parsed = parseFloat(value);
+        if (!isNaN(parsed)) return parsed;
+    }
+    return fallback;
+};
