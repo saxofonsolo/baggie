2a712212ae824ed5c69fce9d6efce493902cd3f2
Lars (Mac) <larsmunkholm@users.noreply.github.com>
2023-11-03 12:47:43 +0100

    config: remove stylelint

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -3,5 +3,4 @@ const sharedTasks = ["prettier --write"];
 // eslint-disable-next-line no-undef
 module.exports = {
     "./packages/**/src/**/*.*(ts|tsx)": ["eslint --max-warnings 1 --fix --cache -c .eslintrc.commit.json -- ", ...sharedTasks],
-    "./packages/**/src/**/*.*(css|scss)": ["stylelint --max-warnings 1 --fix --cache -- ", ...sharedTasks],
 };
