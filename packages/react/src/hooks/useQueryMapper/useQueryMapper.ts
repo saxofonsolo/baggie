1663d31141a05c7a14f9b2868c88efcf62a77bca
Lars (Mac) <larsmunkholm@users.noreply.github.com>
2023-11-16 16:34:08 +0100

    feat: add useQueryMapper

---
+++ packages/react/src/hooks/useQueryMapper/useQueryMapper.ts
@@ -0,0 +1,83 @@
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useSearchParams } from "react-router-dom";
+
+interface Options<T> {
+    defaultValue?: T;
+    history?: "push" | "replace";
+}
+
+export type QueryMapperParams = [key: string, value: string][];
+
+export function useQueryMapper<T>(
+    valueToParams: (value: T) => QueryMapperParams,
+    paramsToValue: (params: QueryMapperParams) => T,
+    options?: Options<T>,
+): [T, (value: T | ((prev: T) => T)) => void] {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const state = useMemo(() => {
+        const entries = Array.from(searchParams.entries());
+        return paramsToValue(entries);
+    }, [paramsToValue, searchParams]);
+
+    const stateForComparison = useRef<T>(state);
+    const paramsForComparison = useRef(Array.from(searchParams.entries()));
+    const applyDefaultValues = useRef(!searchParams.size);
+
+    const setParams = useCallback(
+        (newParams: QueryMapperParams) => {
+            setSearchParams(
+                (prev) => {
+                    const params = Object.fromEntries(prev);
+                    newParams.forEach((newParam) => {
+                        const [key, value] = newParam;
+                        if (value === null) delete params[key];
+                        else params[key] = value;
+                    });
+                    return Object.entries(params);
+                },
+                {
+                    replace: options?.history !== "push",
+                    preventScrollReset: true,
+                },
+            );
+        },
+        [options?.history, setSearchParams],
+    );
+
+    const setState = useCallback(
+        (value: T | ((prev: T) => T)) => {
+            const newValue =
+                typeof value === "function"
+                    ? (value as (prev: T) => T)(stateForComparison.current)
+                    : value;
+            const newParams = valueToParams(newValue);
+
+            // Check if params have changed
+            const haveParamsChanged =
+                newParams.length !== paramsForComparison.current.length ||
+                !paramsForComparison.current.every(([keyA, valueA]) =>
+                    newParams.find(
+                        ([keyB, valueB]) => keyB === keyA && valueA === valueB,
+                    ),
+                );
+
+            // Only update if params have changed
+            if (haveParamsChanged) {
+                setParams(newParams);
+                paramsForComparison.current = newParams;
+                stateForComparison.current = newValue;
+            }
+        },
+        [setParams, valueToParams],
+    );
+
+    useEffect(() => {
+        if (applyDefaultValues.current && options?.defaultValue) {
+            setState(options.defaultValue);
+        }
+        applyDefaultValues.current = false;
+    }, [options, setState]);
+
+    return [state, setState];
+}
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -11,5 +11,7 @@ export { InputWrapper } from "./wrappers/InputWrapper/InputWrapper";
 // Hooks
 export { useForwardedRef } from "./hooks/useForwardedRef/useForwardedRef";
 export { useKeyboardHeight } from "./hooks/useKeyboardHeight/useKeyboardHeight";
+export type { QueryMapperParams } from "./hooks/useQueryMapper/useQueryMapper";
+export { useQueryMapper } from "./hooks/useQueryMapper/useQueryMapper";
 export { useQueryState } from "./hooks/useQueryState/useQueryState";
 export { useRefreshedToday } from "./hooks/useRefreshedToday/useRefreshedToday";
